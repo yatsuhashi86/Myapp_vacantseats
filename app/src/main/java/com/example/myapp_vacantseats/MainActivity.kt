996f6cf051ae544ad1c7b9fd64c0d39ccfe54229
yatsuhashi86 <tomo38yatsuhashi@gmail.com>
2021-01-31 04:28:18 +0900

    入力された文字を取得しただけ

--- app/src/main/java/com/example/myapp_vacantseats/MainActivity.kt
+++ app/src/main/java/com/example/myapp_vacantseats/MainActivity.kt
@@ -15,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         override fun onClick(view: View) {
             val inputCurrentSta = findViewById<EditText>(R.id.enterStartSta)
             val currentSta = inputCurrentSta.text.toString()
+            val inputArriveSta = findViewById<EditText>(R.id.enterEndSta)
+            val arriveSta = inputArriveSta.text.toString()
+            val inputHour = findViewById<EditText>(R.id.jikann)
+            val hour = inputHour.text.toString().toInt()
+            val inputMinute = findViewById<EditText>(R.id.nanhun)
+            val minute = inputMinute.text.toString().toInt()
 
         }
     }
