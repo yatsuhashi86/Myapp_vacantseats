717ec59ea952d8012b6eabd03f491c167e1692cb
yatsuhashi86 <tomo38yatsuhashi@gmail.com>
2021-02-26 03:25:48 +0900

    csvを取り込んで時刻表データをmutablelistに入れる関数を作ったよ。 問題は、31行目に固有の路線の配列を使ってるせいで関数にした意味がないことだよ。

--- app/src/main/java/com/example/myapp_vacantseats/MainActivity.kt
+++ app/src/main/java/com/example/myapp_vacantseats/MainActivity.kt
@@ -4,11 +4,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var syuppatuSta = emptyArray<String>()
+        var toutyakuSta = emptyArray<String>()
+        val koubeSubwayKaiganToEast = MutableList(100){ MutableList(10){ -1 } }
+        fun readCsv(filename: String){
+            try{
+                val file = resources.assets.open(filename)
+                val fileReader = BufferedReader(InputStreamReader(file))
+                var i = 0
+                fileReader.forEachLine {
+                    if (it.isNotBlank()){
+                        if (i == 0){
+                            syuppatuSta = it.split(",").toTypedArray()
+                        } else if (i == 1){
+                            toutyakuSta = it.split(",").toTypedArray()
+                        } else {
+                            val line = it.split(",").map { it.toInt() }.toMutableList()
+                            koubeSubwayKaiganToEast[i] = line
+                        }
+                    }
+                    i++
+                }
+            } catch (e: IOException){
+                println(e)
+            }
+
+        }
+
     }
 
     private inner class TextListener : View.OnClickListener {
@@ -24,6 +54,6 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
-    
+
 
 }
