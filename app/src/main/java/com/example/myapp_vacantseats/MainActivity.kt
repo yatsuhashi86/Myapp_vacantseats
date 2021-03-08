58d27be3ebe13bee8e791de5ba8e1c3c5ff75894
yatsuhashi86 <tomo38yatsuhashi@gmail.com>
2021-03-09 03:41:38 +0900

    一度kariListにデータを代入してあとからそれにかえていく方法にしたよ listが全部varになったのがすごい怖い さらにいうと、路線情報を毎回立ち上げるたびに取得するのは何がなんでもバカすぎると思ったよ。 他のところで実行した結果だけをコピペしたほうがいいと思うよ。

--- app/src/main/java/com/example/myapp_vacantseats/MainActivity.kt
+++ app/src/main/java/com/example/myapp_vacantseats/MainActivity.kt
@@ -16,21 +16,42 @@ data class StationInfo(
 
 
 class MainActivity : AppCompatActivity() {
+
+    var kariList: MutableList<StationInfo> = mutableListOf()
+    var kariSyuppatuSta: MutableList<String> = mutableListOf()
+    var kariToutyakuSta: MutableList<String> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var syuppatuSta = emptyArray<String>()
-        var toutyakuSta = emptyArray<String>()
 
-        val kobeSubwayKaiganToEast: MutableList<StationInfo> = mutableListOf()
 
+
+
+        //海岸線の時刻表データ
+        var kobeSubwayKaiganToEastWeekdays: MutableList<StationInfo> = mutableListOf()
+        var kobeSubwayKaiganToEastWeekends: MutableList<StationInfo> = mutableListOf()
+        var kobeSubwayKaiganToWestWeekdays: MutableList<StationInfo> = mutableListOf()
+        var kobeSubwayKaiganToWestWeekends: MutableList<StationInfo> = mutableListOf()
+
+        //西神線の時刻表データ
+        var kobeSubwaySeishinToEastWeekdays: MutableList<StationInfo> = mutableListOf()
+        var kobeSubwaySeishinToEastWeekends: MutableList<StationInfo> = mutableListOf()
+        var kobeSubwaySeishinToWestWeekdays: MutableList<StationInfo> = mutableListOf()
+        var kobeSubwaySeishinToWestWeekends: MutableList<StationInfo> = mutableListOf()
+
+        //海岸線の週末ダイヤの時刻表データを入れた。hが週末なので注意
+        readCsv("open_kaigan_h_east.csv")
+        kobeSubwayKaiganToEastWeekends = kariList
 
 
     }
 
-
-
-    fun readCsv(filename: String, list: MutableList<StationInfo>, syuppatuSta: MutableList<String>, toutyakuSta: MutableList<String>){
+    //路線データのcsvをkariListにつっこんで行く関数
+    fun readCsv(filename: String){
+        kariList = mutableListOf()
+        kariSyuppatuSta = mutableListOf()
+        kariToutyakuSta = mutableListOf()
         try{
             val file = resources.assets.open(filename)
             val fileReader = BufferedReader(InputStreamReader(file))
@@ -40,28 +61,33 @@ class MainActivity : AppCompatActivity() {
                     if (i == 0){
                         val x = it.split(",").toTypedArray()
                         x.forEach {
-                            syuppatuSta.add(it)
+                            kariSyuppatuSta.add(it)
                         }
                     } else if (i == 1){
                         val x = it.split(",").toTypedArray()
                         x.forEach {
-                            toutyakuSta.add(it)
+                            kariToutyakuSta.add(it)
                         }
                     } else {
                         val line = it.split(",")
                         val x = line[0]
                         val y = line.drop(1).map { it.toInt() }
                         val addInfo = StationInfo(name = x, departureTimes = y)
-                        list.add(addInfo)
+                        kariList.add(addInfo)
                     }
                 }
                 i++
             }
+
         } catch (e: IOException){
             println(e)
         }
 
     }
+
+
+
+
 
     private inner class TextListener : View.OnClickListener {
         override fun onClick(view: View) {
